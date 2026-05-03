044a9477bd9318620803a65da03a74404bfd866e
James Woolfenden <james.woolfenden@gmail.com>
2026-05-03 19:30:11 +0100

    fix: loosen provider version constraints, remove deprecated template provider

--- example/examplea/terraform.tf
+++ example/examplea/terraform.tf
@@ -1,24 +1,20 @@
 terraform {
   required_providers {
     aws = {
-      version = "4.6.0"
       source  = "hashicorp/aws"
+      version = ">= 4.6.0"
     }
     http = {
       source  = "hashicorp/http"
-      version = "2.0.0"
-    }
-    template = {
-      source  = "hashicorp/template"
-      version = "2.2.0"
+      version = ">= 2.0.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "3.0.0"
+      version = ">= 3.0.0"
     }
     local = {
       source  = "hashicorp/local"
-      version = "2.0.0"
+      version = ">= 2.0.0"
     }
   }
   required_version = ">=0.14.8"
