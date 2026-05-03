c67e5b3edd03b319b866eca9d4054e745a885801
James Woolfenden <james.woolfenden@gmail.com>
2026-05-03 10:25:07 +0100

    chore: pin dependencies to immutable SHAs via ghat

--- example/examplea/module.ip.tf
+++ example/examplea/module.ip.tf
@@ -1,4 +1,3 @@
 module "ip" {
-  source  = "JamesWoolfenden/ip/http"
-  version = "0.2.10"
+  source = "git::https://github.com/JamesWoolfenden/terraform-http-ip.git?ref=08d9203ae70ceea9edcd1b2571af4b52c9e5c1db" #v0.2.10
 }
